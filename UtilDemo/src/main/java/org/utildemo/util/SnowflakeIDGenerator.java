6907cd23a922ad90b7d3edb247e2d568a94a720d
Le1eaf <tfz1485413742@gmail.com>
2025-07-30 15:03:30 +0800

    工具Demo

---
+++ UtilDemo/src/main/java/org/utildemo/util/SnowflakeIDGenerator.java
@@ -0,0 +1,12 @@
+package org.utildemo.util;
+
+/**
+ * SnowflakeIDGenerator 枚举/类/接口说明
+ *
+ * @author: tanfuzeng
+ * @date: 2025/7/30 14:57
+ * @version: v1.0.0
+ * @since: 2025/7/30 14:57
+ **/
+public class SnowflakeIDGenerator {
+}
